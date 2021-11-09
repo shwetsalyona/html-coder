5d3e6bf91abae0084c9e701d771cd91027a514b6
andrew.shwets <andrew.shwets@thinkon.com>
2021-11-09 11:26:36 -0500

    dd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app">
+        <p>usum rutrum orc, neque id cursus fauciPellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metusbus, tortors</p>
+        <img  className="cat" src="https://image.shutterstock.com/image-photo/large-overweight-tongue-out-licking-600w-494963905.jpg" alt="Fat cat"/>
+      </div>
+      <div className="container">
+        <img className="kittens" src="https://image.shutterstock.com/shutterstock/photos/1642990687/display_1500/stock-photo-a-grey-kitten-lies-on-a-white-background-cute-kitten-british-cat-cover-for-an-album-or-notebook-1642990687.jpg" alt="2 gray kittens"/>
+        <div className="text">Gray Kittens</div>
+      </div>
     </div>
   );
 }
